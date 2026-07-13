53bdfe41b8e26ec7ed318faba099a7b3134458a7
Sang-Druk <sangdrukclinic@gmail.com>
2026-07-14 01:07:27 +0530

    Update WhoWeAre.tsx

--- src/pages/WhoWeAre.tsx
+++ src/pages/WhoWeAre.tsx
@@ -9,7 +9,7 @@ import directorImg from "@/assets/director.webp";
 import sliderMenlha from "@/assets/whoooo.png";//
 import clove from "@/assets/clove.jpeg";
 import imag3 from "@/assets/imag3.jpeg";
-import aru from "@/assets/Aru.jpeg";
+import aru from "@/assets/Aru.png";
 import calcite from "@/assets/calcite.jpeg";
 import saffron from "@/assets/saffron.jpeg";
 import nutmeg from "@/assets/nutmeg.png";
